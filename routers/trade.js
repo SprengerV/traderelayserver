ff05ebaa7b90a3e3f81cf049fd1b40abde4f2a1d
Brendan Francis <B.Francis@SprengerV.com>
2024-09-11 13:34:12 -0400

    added post route

---
+++ routers/trade.js
@@ -0,0 +1,13 @@
+const app = require('express');
+const path = require('path');
+const { addoif } = require(path.join(__dirname, 'controllers', 'addoif.js'))
+
+const router = app.Router();
+
+router.post('/', (req, res, next) => {
+    const json = JSON.parse(res);
+    const data = json.data.data;
+    addoif(data);
+})
+
+module.exports = router;
