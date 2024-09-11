ea95d803c6e44f9b7dbc4912478c2705056edba4
Brendan Francis <B.Francis@SprengerV.com>
2024-09-11 14:46:25 -0400

    removed NINJA_DIRECTORY

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const trade = require(path.join(__dirname, 'routers', 'trade.js'))
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const nd = process.env.NINJA_DIRECTORY;
 const app = express();
 
 app.get('/', (req, res) => {
