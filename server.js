be79a14a9e4272fdcbcab91b9c26d3ab5b580754
Brendan Francis <B.Francis@SprengerV.com>
2024-09-11 13:37:24 -0400

    fixed dotenv and added trade router

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-require('dotenv').configure();
+const path = require('path');
+const trade = require(path.join(__dirname, 'routers', 'trade.js'))
+require('dotenv').config();
 
 const PORT = process.env.PORT;
 const nd = process.env.NINJA_DIRECTORY;
@@ -8,6 +10,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Mind your business.');
 });
+
+app.use('/trade', trade);
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT} !`)
