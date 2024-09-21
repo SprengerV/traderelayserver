2f73ee71f71db18cf63f1ad4e63e94d62c16d634
Brendan Francis <B.Francis@SprengerV.com>
2024-09-21 13:51:18 -0400

    made OIF numbers 6 digits padded with 0s, formatted code to make more readable

--- controllers/addoif.js
+++ controllers/addoif.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const time = require(path.join(__dirname, '..', 'util', 'time.js'))
-require('dotenv').config();
+const fs = require( 'fs' );
+const path = require( 'path' );
+const time = require( path.join( __dirname, '..', 'util', 'time.js' ) )
+require( 'dotenv' ).config();
 
 const dir = process.env.NINJA_DIRECTORY;
 const delay = process.env.DELAY;
@@ -9,10 +9,10 @@ const win = process.env.WINDOW;
 const contracts = {
     'SI1!': 'SI 12-24'
 };
-const tickers = Object.keys(contracts)
+const tickers = Object.keys( contracts )
 
-const addoif = (strat, obj) => {
-    const state = parseInt(obj.position_size);
+const addoif = ( strat, obj ) => {
+    const state = parseInt( obj.position_size );
     const params = {
         'Command': '',
         'Account': '',
@@ -29,38 +29,38 @@ const addoif = (strat, obj) => {
         'Strategy ID': ''
     } 
 
-    params['Command'] = (state ? 'PLACE' : 'CLOSEPOSITION');
-    params['Account'] = strat.account
-    params['Instrument'] = (tickers.includes(obj.ticker) ? contracts[obj.ticker] : '')
-    params['Action'] = (state ? obj.order.toUpperCase() : '');
-    params['Order Type'] = (state ? 'MARKET' : '');
-    params['QTY'] = (state ? parseInt(obj.contracts) : '');
-    params['TIF'] = (state ? 'GTC' : '');
-    params['Strategy'] = (state ? obj.atm : '');
+    params[ 'Command' ] = ( state ? 'PLACE' : 'CLOSEPOSITION' );
+    params[ 'Account' ] = strat.account
+    params[ 'Instrument' ] = ( tickers.includes( obj.ticker ) ? contracts[ obj.ticker ] : '' )
+    params[ 'Action' ] = ( state ? obj.order.toUpperCase() : '' );
+    params[ 'Order Type' ] = ( state ? 'MARKET' : '' );
+    params[ 'QTY' ] = ( state ? parseInt( obj.contracts ) : '' );
+    params[ 'TIF' ] = ( state ? 'GTC' : ''  );
+    params[ 'Strategy' ] = ( state ? obj.atm : '' );
 
-    const keys = Object.keys(params);
+    const keys = Object.keys( params );
     let paramstring = "";
-    keys.forEach((v, i, arr) => {
-        paramstring += params[v] + (i < arr.length -1 ? ";" :"")
+    keys.forEach(( v, i, arr ) => {
+        paramstring += params[ v ] + ( i < arr.length -1 ? ";" :"" )
     });
 
-    fs.readdir(dir,(err, files) => {
-        if (err) {
-            console.log(`${time()} Error checking NinjaTrader disectory!\n${err}`);
+    fs.readdir(dir,( err, files ) => {
+        if ( err ) {
+            console.log( `${ time() } Error checking NinjaTrader disectory!\n${ err }` );
             return null
         }
         const { length } = files;
-        const rand = (state ? (1000 * delay + 1000 * win * Math.random()) : 0);
-        console.log(`${time()} RANDOMIZER: ${ rand / 1000 } seconds`)
-        setTimeout(() => {
-            const fname = `oif${length}.txt`
-            const dest = path.join(dir, fname);
-            console.log(`${time()} Parameters for new OIF: ${paramstring}`);
-            fs.writeFile(dest, paramstring, (err) => {
-                if (err) console.log(err);
-                else console.log(`${time()} ${dest} written!`);
-            });
-        }, rand);
+        const rand = ( state ? ( 1000 * delay + 1000 * win * Math.random() ) : 0 );
+        console.log( `${ time() } RANDOMIZER: ${ rand / 1000 } seconds` )
+        setTimeout( () => {
+            const fname = `oif${ length.toString().padStart( ( 6 - length.toString().length ), "0" ) }.txt`
+            const dest = path.join( dir, fname );
+            console.log( `${ time() } Parameters for new OIF: ${ paramstring }` );
+            fs.writeFile( dest, paramstring, ( err ) => {
+                if ( err ) console.log( err );
+                else console.log( `${ time() } ${ dest } written!` );
+            } );
+        }, rand );
     });
 };
 
