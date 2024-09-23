07bb9a7570dc4bccdf1ef9435cdd4ad1b0385a9d
Brendan Francis <B.Francis@SprengerV.com>
2024-09-23 13:31:02 -0400

    added functions to init, set, and get state

---
+++ util/state.js
@@ -0,0 +1,30 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+const sf = path.join( __dirname, '..', 'config', 'state.json' );
+const fresh = {
+    "position_size": 0,
+    "active_atm": null
+};
+
+const initState = () => {
+    if ( ! fs.existsSync( sf ) ) {
+        fs.writeFileSync( sf, JSON.stringify( fresh, null, 2 ) )
+    }
+};
+
+const getState = () => {
+    initState();
+    const state = JSON.parse( fs.readFileSync( sf ) );
+    if ( state ) state
+    else null
+};
+
+const setState = ( obj ) => {
+    fs.writeFileSync( sf, JSON.stringify( obj, null, 2 ) );
+};
+
+module.exports = {
+    getState,
+    setState
+};
