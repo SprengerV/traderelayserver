ae457b3bc8b3ddde88366af00ed69ec852459aac
Brendan Francis <B.Francis@SprengerV.com>
2024-11-28 01:14:41 -0500

    Error handling

--- util/speak.js
+++ util/speak.js
@@ -1,8 +1,15 @@
-const say = require('say');
+const say = require( 'say' );
 
 
 module.exports = {
-    speakNT: (dir, pos, atm) => {
-        say.speak(`${dir}, ${pos} contracts, ${atm}`);
+    speakNT: ( dir, pos, atm ) => {
+        say.speak(`${ dir }, ${ pos } contracts, ${ atm }`,
+            null, null, ( err ) => {
+                if ( err ) {
+                    console.error( err );
+                    return;
+                }
+                console.log( 'NinjaTrader order announced' );
+        } );
     }
 };
